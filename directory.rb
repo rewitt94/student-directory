7543f23380899efbfcbd08030f98045f7eedcad1
rewitt94 <33395399+rewitt94@users.noreply.github.com>
2018-02-16 08:20:33 +0000

    refactoring

--- directory.rb
+++ directory.rb
@@ -1,3 +1,5 @@
+student_count = 5
+#first we print the list of students
 puts 'The students of Superhero Academy'
 puts '---------------------------------'
 puts 'Batman'
@@ -5,3 +7,5 @@ puts 'Luther'
 puts 'Lara Croft'
 puts 'Rick'
 puts 'Luke Skywalker'
+#finally we print the amount of students
+puts "We have #{student_count} great students"
