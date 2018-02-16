2544ce7adddb4325d668a555764e9430c64b0cc8
rewitt94 <33395399+rewitt94@users.noreply.github.com>
2018-02-16 14:15:18 +0000

    further refactoring

--- directory.rb
+++ directory.rb
@@ -1,68 +1,73 @@
-def input_students(students)
+def print_menu
+  puts 'Select an option'
+  puts '1. Input students'
+  puts '2. List students'
+  puts '9. Exit'
+end
+def input_students
   puts "Please enter the names of students."
   puts "To finish, just hit return twice"
   name = gets.chop
   while !name.empty? do
     puts "In which cohort is #{name}?"
     join_date = gets.chop
-    students << {name: name, cohort: join_date.to_sym}
-    if students.count < 2
-      puts "Now we have #{students.count} student"
+    @students << {name: name, cohort: join_date.to_sym}
+    if @students.count < 2
+      puts "Now we have #{@students.count} student"
     else
-      puts "Now we have #{students.count} students"
+      puts "Now we have #{@students.count} students"
     end
     name = gets.chop
   end
-  students
 end
 def print_header
   puts 'The students of Superhero Academy'
   puts '---------------------------------'
 end
-def print(students)
+def print_students_list
   cohorts = []
-  students.each { |student|
+  @students.each { |student|
     cohorts.push(student[:cohort])
   }
   cohorts = cohorts.uniq
   cohorts.each do |cohort|
     puts "Joining the #{cohort} cohort:"
     index = 0
-    while index < students.length do
-      if cohort == students[index][:cohort]
-        puts "#{index + 1}. #{students[index][:name]}"
+    while index < @students.length do
+      if cohort == @students[index][:cohort]
+        puts "#{index + 1}. #{@students[index][:name]}"
       end
       index += 1
     end
   end
 
 end
-def print_footer(students)
-  if students.count < 2
-    puts "We have #{students.count} great student"
+def print_footer
+  if @students.count < 2
+    puts "We have #{@students.count} great student"
   else
-    puts "We have #{students.length} great students"
+    puts "We have #{@students.length} great students"
+  end
+end
+def list_students
+  if @students.length >= 1
+    print_header
+    print_students_list
+    print_footer
+  else
+    puts 'No students to list'
   end
 end
 def interactive_menu
-  students = []
+  @students = []
   loop do
-    puts 'Select an option'
-    puts '1. Input students'
-    puts '2. List students'
-    puts '9. Exit'
+    print_menu
     selection = gets.chomp
     case selection
     when '1'
-      students = input_students(students)
+      input_students
     when '2'
-      if students.length >= 1
-        print_header
-        print(students)
-        print_footer(students)
-      else
-        puts 'No students to list'
-      end
+      list_students
     when '9'
       exit
     else
