0852b8490371903b83e453aef35699397b3ace3b
rewitt94 <33395399+rewitt94@users.noreply.github.com>
2018-02-16 08:42:14 +0000

    more complex

--- directory.rb
+++ directory.rb
@@ -1,23 +1,23 @@
 students = [
-  'Black Panther',
-  'Luther',
-  'Lara Croft',
-  'Rick',
-  'Rey',
-  'Harry Potter',
-  'Legolas'
+  {name: 'Black Panther', cohort: :febuary},
+  {name: 'Luther', cohort: :may},
+  {name: 'Lara Croft', cohort: :march},
+  {name: 'Rick', cohort: :january},
+  {name: 'Rey', cohort: :december},
+  {name: 'Harry Potter', cohort: :febuary},
+  {name: 'Legolas', cohort: :december}
 ]
 def print_header
   puts 'The students of Superhero Academy'
   puts '---------------------------------'
 end
-def print(names)
-  names.each { |student|
-  puts student
+def print(students)
+  students.each { |student|
+  puts "#{student[:name]} (#{student[:cohort]} cohort)"
 }
 end
-def print_footer(names)
-  puts "We have #{names.count} great students"
+def print_footer(students)
+  puts "We have #{students.length} great students"
 end
 print_header
 print(students)
