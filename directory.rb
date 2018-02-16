b769025413badee94ed3214563e57c3a7445afe4
rewitt94 <33395399+rewitt94@users.noreply.github.com>
2018-02-16 12:32:42 +0000

    looking good

--- directory.rb
+++ directory.rb
@@ -1,12 +1,18 @@
 def input_students
-  puts "Please enter the names of the students"
+  puts "Please enter the names of students."
   puts "To finish, just hit return twice"
   students = []
-  name = gets.chomp
+  name = gets.chop
   while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
-    name = gets.chomp
+    puts "In which cohort is #{name}?"
+    join_date = gets.chop
+    students << {name: name, cohort: join_date.to_sym}
+    if students.count < 2
+      puts "Now we have #{students.count} student"
+    else
+      puts "Now we have #{students.count} students"
+    end
+    name = gets.chop
   end
   students
 end
@@ -15,14 +21,33 @@ def print_header
   puts '---------------------------------'
 end
 def print(students)
+  cohorts = []
   students.each { |student|
-  puts "#{student[:name]} (#{student[:cohort]} cohort)"
-}
+    cohorts.push(student[:cohort])
+  }
+  cohorts = cohorts.uniq
+  cohorts.each do |cohort|
+    puts "Joining the #{cohort} cohort:"
+    index = 0
+    while index < students.length do
+      if cohort == students[index][:cohort]
+        puts "#{index + 1}. #{students[index][:name]}"
+      end
+      index += 1
+    end
+  end
+
 end
 def print_footer(students)
-  puts "We have #{students.length} great students"
+  if students.count < 2
+    puts "We have #{students.count} great student"
+  else
+    puts "We have #{students.length} great students"
+  end
 end
 students = input_students
-print_header
-print(students)
-print_footer(students)
+if students > 1
+  print_header
+  print(students)
+  print_footer(students)
+end
