051c977409d4391d9521a8d4b25074f0d4e0eb14
rewitt94 <33395399+rewitt94@users.noreply.github.com>
2018-02-16 08:54:12 +0000

    input introduced

--- directory.rb
+++ directory.rb
@@ -1,12 +1,15 @@
-students = [
-  {name: 'Black Panther', cohort: :febuary},
-  {name: 'Luther', cohort: :may},
-  {name: 'Lara Croft', cohort: :march},
-  {name: 'Rick', cohort: :january},
-  {name: 'Rey', cohort: :december},
-  {name: 'Harry Potter', cohort: :febuary},
-  {name: 'Legolas', cohort: :december}
-]
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  students = []
+  name = gets.chomp
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+end
 def print_header
   puts 'The students of Superhero Academy'
   puts '---------------------------------'
@@ -19,6 +22,7 @@ end
 def print_footer(students)
   puts "We have #{students.length} great students"
 end
+students = input_students
 print_header
 print(students)
 print_footer(students)
