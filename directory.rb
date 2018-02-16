9dd7dba814b006ed8a2fb632899022299cc7cea6
rewitt94 <33395399+rewitt94@users.noreply.github.com>
2018-02-16 08:31:42 +0000

    separated into methods

--- directory.rb
+++ directory.rb
@@ -1,11 +1,24 @@
-student_count = 5
-#first we print the list of students
-puts 'The students of Superhero Academy'
-puts '---------------------------------'
-puts 'Batman'
-puts 'Luther'
-puts 'Lara Croft'
-puts 'Rick'
-puts 'Luke Skywalker'
-#finally we print the amount of students
-puts "We have #{student_count} great students"
+students = [
+  'Black Panther',
+  'Luther',
+  'Lara Croft',
+  'Rick',
+  'Rey',
+  'Harry Potter',
+  'Legolas'
+]
+def print_header
+  puts 'The students of Superhero Academy'
+  puts '---------------------------------'
+end
+def print(names)
+  names.each { |student|
+  puts student
+}
+end
+def print_footer(names)
+  puts "We have #{names.count} great students"
+end
+print_header
+print(students)
+print_footer(students)
